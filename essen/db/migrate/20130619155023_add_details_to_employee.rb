e007108aa51c406a144ff98ae5dcd3255306d479
Mario Somrei <github@gmx.de>
2013-06-27 17:42:01 +0300

    migration angepasst

--- essen/db/migrate/20130619155023_add_details_to_employee.rb
+++ essen/db/migrate/20130619155023_add_details_to_employee.rb
@@ -1,10 +1,5 @@
 class AddDetailsToEmployee < ActiveRecord::Migration
   def change
     add_column :employees, :valid_to, :datetime
-    add_column :employees, :guest, :boolean
-    add_column :employees, :number_of_children_level_1, :integer
-    add_column :employees, :number_of_children_level_2, :integer
-    add_column :employees, :number_of_children_level_3, :integer
-    add_column :employees, :default_number_of_meals_adults, :datetime
   end
 end
