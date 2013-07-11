25f3f0ea0d645f9889b8d8cfae8922c7fd027720
Mario Somrei <github@gmx.de>
2013-07-11 23:43:28 +0300

    auswertung rechnung

---
+++ essen/app/controllers/invoices_controller.rb
@@ -0,0 +1,27 @@
+class InvoicesController < ApplicationController
+
+  def new
+    @invoice = Invoice.new
+    
+    respond_to do |format|
+      format.html # new.html.erb
+    end
+  end
+
+  def create
+    start_of_month_date = params["invoice"].map{|k,v| v}.join("-").to_date
+    end_of_month_date = start_of_month_date.end_of_month
+    @month = start_of_month_date.strftime("%B") 
+    @meal_plan_entries_per_month = meal_plan_entries_for_month_per_employee(start_of_month_date, end_of_month_date)
+  end
+
+  private
+
+  def meal_plan_entries_for_month_per_employee(start_date, end_date)
+    MealPlanEntry.joins("JOIN employees ON employees.id = employee_id")
+                 .where("date >= ? AND date <= ?", start_date, end_date)
+                 .group(:employee_id)
+                 .select('employee_id, firstname, lastname, SUM(number_of_breakfasts) as sum_breakfasts, SUM(number_of_lunches) as sum_lunches, SUM(number_of_suppers) as sum_suppers')
+  end
+
+end
--- essen/config/routes.rb
+++ essen/config/routes.rb
@@ -1,15 +1,8 @@
 Essen::Application.routes.draw do
   
-#  get "foods/:id" => "foods#show", :as => :food
-
   get "meal_plans/index_delete" => "meal_plans#index_delete"
-  get "reports/show_monthly_invoice_report" => "reports#show_monthly_invoice_report"
-  get "reports/show_daily_cooking_report" => "reports#show_daily_cooking_report"
-  get "reports/create_monthly_invoice_report" => "reports#create_monthly_invoice_report"
-  get "reports/create_daily_cooking_report" => "reports#create_daily_cooking_report"
-#  get "meal_plans/show"
-#  get "meal_plans/delete"
 
+  resources :invoices
   resources :employees
   resources :meal_plan_entries
   resources :meal_plans
